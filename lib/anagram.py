0f6963fbda2d96acef76c957fd9ceaa3eb968a3d
Jacob <jacobpayne007@gmail.com>
2024-09-26 13:04:29 -0500

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        anagrams = []
+        for item in list:
+            if ''.join(sorted(item)) == ''.join(sorted(self.word)):
+                anagrams.append(item)
+                print(anagrams)
+        return anagrams
+            
+            
+
+listen = Anagram('listen')
+
+listen.match(['enlists', 'google', 'inlets', 'banana','listen'])
